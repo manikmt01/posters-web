55c8e849329c313a4051b874a6f0bc1346cc10f6
zellyo-digital0 <zellyo.digital@gmail.com>
2025-01-28 00:50:32 +0600

    tarunno-login

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+// Selecting necessary elements
+const fileInput = document.getElementById('file-upload');
+const photoPreview = document.querySelector('.uploaded-photo-preview');
+const noPhotoText = document.querySelector('.uploaded-nophoto-text');
+const deleteIcon = document.querySelector('.delete-photo-icon');
+
+fileInput.addEventListener('change', function () {
+  const file = this.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      // Set uploaded image as background
+      photoPreview.style.backgroundImage = `url(${e.target.result})`;
+      photoPreview.style.backgroundSize = 'cover';
+      photoPreview.style.backgroundPosition = 'center';
+    };
+    reader.readAsDataURL(file);
+
+   
+    noPhotoText.style.display = 'none';
+    deleteIcon.style.background = '#fff';
+  }
+});
+
+
+deleteIcon.addEventListener('click', function () {
+
+  photoPreview.style.backgroundImage = 'none';
+
+
+  noPhotoText.style.display = 'block';
+  deleteIcon.style.background = 'transparent';
+
+  fileInput.value = '';
+});
+
+
